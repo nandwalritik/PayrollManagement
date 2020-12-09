c26aaefebcf88d7fbbb8b463c9571cdcbaff967d
nandwalritik <ritiknandwal021@gmail.com>
2020-12-09 20:10:54 +0530

    Add quries for deleting employee,getting all employees and emloyee login

--- backend/src/queries.js
+++ backend/src/queries.js
@@ -62,7 +62,8 @@ const adminLogin = async (req, res) => {
   }
 };
 const createEmployee = async (req, res) => {
-  const query = `INSERT INTO Employee(paid_leave_taken,encashed_leave_this_month,encashed_leave_till_date,e_id,doj,name,dob,age,years_of_service,address,city,state,pincode) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) returning *`;
+  const query = `INSERT INTO Employee(paid_leave_taken,encashed_leave_this_month,encashed_leave_till_date,e_id,doj,name,dob,age,years_of_service,address,city,state,pincode,email,password,org_name,dept_id,grade_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18) returning *`;
+  const hashPassword = Helper.hashPassword(req.body.password);
   const values = [
     req.body.paid_leave_taken,
     req.body.encashed_leave_this_month,
@@ -77,6 +78,11 @@ const createEmployee = async (req, res) => {
     req.body.city,
     req.body.state,
     req.body.pincode,
+    req.body.email,
+    hashPassword,
+    req.body.org_name,
+    req.body.dept_id,
+    req.body.grade_id,
   ];
   try {
     const { rows } = await db.query(query, values);
@@ -90,7 +96,68 @@ const createEmployee = async (req, res) => {
         .status(400)
         .send({ message: "Email address is already taken" });
     }
-    return res.status(400).send(err);
+    return res.status(400).send({ error: err });
   }
 };
-module.exports = { createAdmin, adminLogin, createEmployee };
+const deleteEmployee = async (req, res) => {
+  const query = `DELETE FROM Employee WHERE e_id = $1`;
+  try {
+    const { rows } = await db.query(query, [req.body.e_id]);
+    return res.status(200).send({ message: "User.Deleted" });
+  } catch (err) {
+    return res.status(400).send({ error: err });
+  }
+};
+const getAllEmployees = async (req, res) => {
+  const query = `SELECT * from employee`;
+  try {
+    const { rows } = await db.query(query);
+    // console.log(rows);
+    return res.status(200).send({ message: "All Employees", data: rows });
+  } catch (err) {
+    return res.status(400).send({ error: err });
+  }
+};
+const employeeLogin = async(req,res)=>{
+  console.log(req);
+  if (!req.body.email || !req.body.password) {
+    return res
+      .status(400)
+      .send({ message: "Please enter a valid email address" });
+  }
+  if (!Helper.isValidEmail(req.body.email)) {
+    return res
+      .status(400)
+      .send({ message: "Please enter a valid email address" });
+  }
+  const query = "SELECT * FROM employee where email = $1";
+  try {
+    const { rows } = await db.query(query, [req.body.email]);
+    console.log(rows)
+    if (!rows[0]) {
+      return res
+        .status(400)
+        .send({ message: "The credentials you provided is incorrect" });
+    }
+    if (!Helper.comparePassword(rows[0].password, req.body.password)) {
+      return res
+        .status(400)
+        .send({ message: "The credentials you provided is incorrect" });
+    }
+    const token = Helper.generateToken(rows[0].email);
+    return res.status(200).send({ message: "Auth.verified", token: token });
+  } catch (error) {
+    return res.status(400).send({ message: error });
+  }
+}
+// const updateEmployeedata = async(req,res)=>{
+//   const query 
+// }
+module.exports = {
+  createAdmin,
+  adminLogin,
+  createEmployee,
+  deleteEmployee,
+  getAllEmployees,
+  employeeLogin
+};
