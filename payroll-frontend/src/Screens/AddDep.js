5042c872999f118a84dc728a72ba6f833f29ec93
nandwalritik <ritiknandwal021@gmail.com>
2020-12-17 23:27:02 +0530

    Add api connections for adding department

--- payroll-frontend/src/Screens/AddDep.js
+++ payroll-frontend/src/Screens/AddDep.js
@@ -2,75 +2,124 @@ import React, { useState } from "react";
 import logo from "../Assets/Logo.png";
 import "../StyleSheets/Welcome.css";
 import "../StyleSheets/AdminOptions.css";
+import { useFormik } from "formik";
+import { base_url } from "../baseUrl";
+import { useHistory } from "react-router-dom";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
 
-
-const AddDep=()=>{
-  const [dep,setdep]=useState({dep_ID:'',dep_Name:'',org_Name:''})
-  const [people, setPeople] = useState([]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setPeople([...people,dep])
-
-    setdep({dep_ID:'',dep_Name:'',org_Name:''})
+const AddDep = () => {
+  const [open, setOpen] = useState(false);
+  const [msg, setMessage] = useState("");
+  const history = useHistory();
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  const handleChange=(e)=>{
-    const name=e.target.name;
-    const value=e.target.value;
-
-    setdep({...dep,[name]:value})
-  }
+  const handleClose = () => {
+    setOpen(false);
+    history.goBack();
+  };
+  const initialValues = {
+    dept_name: "",
+    org_name: "",
+  };
+  const onSubmit = async (values) => {
+    console.log(values);
+    const body = JSON.stringify(values);
+    fetch(`${base_url}addDepartment`, {
+      method: "POST",
+      body: body,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setMessage(res.message);
+        handleClickOpen();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
   return (
     <>
-      <div className='App'>
+      <div className="App">
+        <div>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{"Message"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {msg}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary" autoFocus>
+                OK
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
         <header>
           <h1>Payroll Management System</h1>
           <hr className="Underline" />
           <img src={logo} alt="logo"></img>
         </header>
-        <form className='form' onSubmit={handleSubmit}>
-              <div className='form-control'>
-              <label htmlFor='dep_ID'>Dep ID : </label>
-              <input
-                type='number'
-                id='dep_ID'
-                name='dep_ID'
-                value={dep.dep_ID}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className='form-control'>
-              <label htmlFor='dep_Name'>Dep Name : </label>
-              <input
-                type='text'
-                id='dep_Name'
-                name='dep_Name'
-                value={dep.dep_Name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            
-            <div className='form-control'>
-              <label htmlFor='org_Name'>Org Name : </label>
-              <input
-                type='text'
-                id='org_Name'
-                name='org_Name'
-                value={dep.org_Name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            
-          <button type='submit'>add department</button>
+        <form className="form" onSubmit={formik.handleSubmit}>
+          {/* <div className="form-control">
+            <label htmlFor="dep_ID">Dep ID : </label>
+            <input
+              type="number"
+              id="dep_ID"
+              name="dep_ID"
+              value={dep.dep_ID}
+              onChange={handleChange}
+              required
+            />
+          </div> */}
+          <div className="form-control">
+            <label htmlFor="dep_Name">Dep Name : </label>
+            <input
+              type="text"
+              id="dep_Name"
+              name="dep_Name"
+              value={formik.values.dept_name}
+              onChange={formik.handleChange("dept_name")}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="org_Name">Org Name : </label>
+            <input
+              type="text"
+              id="org_Name"
+              name="org_Name"
+              value={formik.values.org_name}
+              onChange={formik.handleChange("org_name")}
+              required
+            />
+          </div>
+          <button type="submit">add department</button>
         </form>
-        
       </div>
     </>
   );
-}
+};
 
 export default AddDep;
