20fea848ffb4716d66bd6ac4414000bacc245e7b
nandwalritik <ritiknandwal021@gmail.com>
2020-12-17 23:27:25 +0530

    Add api connections for adding employee

--- payroll-frontend/src/Screens/AddEmployee.js
+++ payroll-frontend/src/Screens/AddEmployee.js
@@ -1,179 +1,203 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../StyleSheets/Welcome.css";
 import "../StyleSheets/AdminOptions.css";
 import { useFormik } from "formik";
+import { base_url } from "../baseUrl";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
+
 const AddEmployee = () => {
-  // console.log(props);
-  // if (typeof props === "undefined")
-  //   props = {
-  //     Name: "",
-  //     ID: "",
-  //     DOB: "",
-  //     City: "",
-  //     State: "",
-  //     Pincode: "",
-  //     Address: "",
-  //     Dept_ID: "",
-  //     Grade_ID: "",
-  //     OrgName: "",
-  //     DOJ: "",
-  //     PaidLeaveTaken: "",
-  //     EncashedLeaveThisMonth: "",
-  //     EncashedLeaveTillDate: "",
-  //   };
+  const [departmentsList, setDeplist] = useState([]);
+  const [gradesList, setGradeslist] = useState([]);
+  const history = useHistory();
+  const [open, setOpen] = useState(false);
+  const [msg, setMessage] = useState("");
 
-  // const [person, setPerson] = useState({
-  //   Name: props.Name,
-  //   ID: props.ID,
-  //   DOB: props.DOB,
-  //   City: props.City,
-  //   State: props.State,
-  //   Pincode: props.PinCode,
-  //   Address: props.Address,
-  //   Dept_ID: props.Dept_ID,
-  //   Grade_ID: props.Grade_ID,
-  //   OrgName: props.OrgName,
-  //   DOJ: props.DOJ,
-  //   PaidLeaveTaken: props.PaidLeaveTaken,
-  //   EncashedLeaveThisMonth: props.EncashedLeaveThisMonth,
-  //   EncashedLeaveTillDate: props.EncashedLeaveTillDate,
-  // });
-  // const [people, setPeople] = useState([]);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setPeople([...people, person]);
-
-  //   setPerson({
-  //     Name: "",
-  //     ID: "",
-  //     DOB: "",
-  //     City: "",
-  //     State: "",
-  //     Pincode: "",
-  //     Address: "",
-  //     Dept_ID: "",
-  //     Grade_ID: "",
-  //     OrgName: "",
-  //     DOJ: "",
-  //     PaidLeaveTaken: "",
-  //     EncashedLeaveThisMonth: "",
-  //     EncashedLeaveTillDate: "",
-  //   });
-  // };
-
-  // const handleChange = (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-
-  //   setPerson({ ...person, [name]: value });
-  // };
+  const handleClose = () => {
+    setOpen(false);
+    history.goBack();
+  };
   const initialValues = {
-    Name: "",
-    DOB: "",
-    City: "",
-    State: "",
-    Pincode: "",
-    Address: "",
-    Dept_ID: "",
-    Grade_ID: "",
-    OrgName: "",
-    DOJ: "",
-    PaidLeaveTaken: "",
-    EncashedLeaveThisMonth: "",
-    EncashedLeaveTillDate: "",
+    name: "",
+    dob: "",
+    city: "",
+    state: "",
+    pincode: "",
+    address: "",
+    dept_id: "",
+    grade_id: "",
+    org_name: "SGSITS",
+    doj: "",
+    email: "",
   };
   const onSubmit = async (values) => {
     console.log(values);
+    const body = JSON.stringify(values);
+    fetch(`${base_url}addEmployee`, {
+      method: "POST",
+      body: body,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setMessage(res.message);
+        handleClickOpen();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
   const formik = useFormik({
     initialValues,
     onSubmit,
   });
-
+  useEffect(() => {
+    getDepartments();
+    getGrades();
+  }, []);
+  const getDepartments = async () => {
+    fetch(`${base_url}getDepartments`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setDeplist(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const getGrades = async () => {
+    fetch(`${base_url}getGrades`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setGradeslist(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const departments = departmentsList.map((dept) => {
+    return <option value={dept.dept_id}>{dept.dept_name}</option>;
+  });
+  const grades = gradesList.map((grade) => {
+    return <option value={grade.grade_id}>{grade.grade_name}</option>;
+  });
   return (
     <>
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Message"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {msg}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            {/* <Button onClick={handleClose} color="primary">
+              Disagree
+            </Button> */}
+            <Button onClick={handleClose} color="primary" autoFocus>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
       <div className="App">
         <form className="form" onSubmit={formik.handleSubmit}>
           <div className="grouping">
             <h3>Basic Details</h3>
             <hr className="Underline" />
             <div className="form-control">
-              <label htmlFor="Name">Name : </label>
+              <label htmlFor="name">Name : </label>
               <input
                 type="text"
-                id="Name"
-                name="Name"
-                value={formik.values.Name}
-                onChange={formik.handleChange("Name")}
+                id="name"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange("name")}
                 required
               />
             </div>
-            {/* <div className="form-control">
-              <label htmlFor="ID">Employee ID : </label>
-              <input
-                type="number"
-                id="ID"
-                name="ID"
-                value={person.ID}
-                onChange={handleChange}
-                required
-              />
-            </div> */}
+
             <div className="form-control">
-              <label htmlFor="DOB">DOB : </label>
+              <label htmlFor="dob">Dob : </label>
               <input
                 type="date"
-                id="DOB"
-                name="DOB"
-                value={formik.values.DOB}
-                onChange={formik.handleChange("DOB")}
+                id="dob"
+                name="dob"
+                value={formik.values.dob}
+                onChange={formik.handleChange("dob")}
                 required
               />
             </div>
             <div className="form-control">
-              <label htmlFor="Address">Address : </label>
+              <label htmlFor="address">Address : </label>
               <input
                 type="text"
-                id="Address"
-                name="Address"
-                value={formik.values.Address}
-                onChange={formik.handleChange("Address")}
+                id="address"
+                name="address"
+                value={formik.values.address}
+                onChange={formik.handleChange("address")}
                 required
               />
             </div>
             <div className="form-control">
-              <label htmlFor="City">City : </label>
+              <label htmlFor="city">City : </label>
               <input
                 type="text"
-                id="City"
-                name="City"
-                value={formik.values.City}
-                onChange={formik.handleChange("City")}
+                id="city"
+                name="city"
+                value={formik.values.city}
+                onChange={formik.handleChange("city")}
                 required
               />
             </div>
             <div className="form-control">
-              <label htmlFor="State">State : </label>
+              <label htmlFor="state">State : </label>
               <input
                 type="text"
-                id="State"
-                name="State"
-                value={formik.values.State}
-                onChange={formik.handleChange("State")}
+                id="state"
+                name="state"
+                value={formik.values.state}
+                onChange={formik.handleChange("state")}
                 required
               />
             </div>
             <div className="form-control">
-              <label htmlFor="Pincode" className="pinCode">
+              <label htmlFor="pincode" className="pincode">
                 Pin-Code :{" "}
               </label>
               <input
                 type="number"
-                id="Pincode"
-                name="Pincode"
-                value={formik.values.Pincode}
-                onChange={formik.handleChange("Pincode")}
+                id="pincode"
+                name="pincode"
+                value={formik.values.pincode}
+                onChange={formik.handleChange("pincode")}
                 size="6"
                 maxLength="6"
               />
@@ -184,46 +208,59 @@ const AddEmployee = () => {
             <h3>Company Details</h3>
             <hr className="Underline" />
             <div className="form-control">
-              <label htmlFor="Dept">Department: </label>
-              <input
-                type="number"
-                id="Dept"
-                name="Dept"
-                // value={person.Dept_ID}
-                // onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="Grade_ID">Grade-ID : </label>
-              <input
-                type="number"
-                id="Grade_ID"
-                name="Grade_ID"
-                // value={person.Grade_ID}
-                // onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label htmlFor="OrgName">Org Name: </label>
+              <label htmlFor="org_name">Email : </label>
               <input
                 type="text"
-                id="OrgName"
-                name="OrgName"
-                // value={person.OrgName}
-                // onChange={handleChange}
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange("email")}
                 required
               />
             </div>
             <div className="form-control">
-              <label htmlFor="DOJ">DOJ : </label>
+              <label htmlFor="Dept">Department: </label>
+              <select
+                style={styles.dropDown}
+                value={formik.values.dept_id}
+                onChange={formik.handleChange("dept_id")}
+                required
+              >
+                {departments}
+              </select>
+            </div>
+            <div className="form-control">
+              <label htmlFor="grade_id">Grade-ID : </label>
+              <select
+                style={styles.dropDown}
+                value={formik.values.grade_id}
+                onChange={formik.handleChange("grade_id")}
+                required
+              >
+                {grades}
+              </select>
+            </div>
+            <div className="form-control">
+              <label htmlFor="org_name">Org Name: </label>
               <input
+                type="text"
+                id="org_name"
+                name="org_name"
+                value={formik.values.org_name}
+                // onChange={handleChange}
+                disabled={true}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="doj">doj : </label>
+              <input
+                // style={styles.input}
                 type="date"
-                id="DOJ"
-                name="DOJ"
-                value={formik.values.DOJ}
-                onChange={formik.handleChange("DOJ")}
+                id="doj"
+                name="doj"
+                value={formik.values.doj}
+                onChange={formik.handleChange("doj")}
                 required
               />
             </div>
@@ -237,4 +274,23 @@ const AddEmployee = () => {
   );
 };
 
+const styles = {
+  input: {
+    width: window.innerWidth / 4.5,
+    height: "70%",
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "black",
+    marginTop: "1%",
+  },
+  dropDown: {
+    width: window.innerWidth / 4.4,
+    height: window.innerHeight / 25,
+    backgroundColor: "hsl(212deg 33% 89%)",
+    borderWidth: 0,
+    borderColor: "black",
+    borderRadius: 5,
+    marginTop: "1%",
+  },
+};
 export default AddEmployee;
